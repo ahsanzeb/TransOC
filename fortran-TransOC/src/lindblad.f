1a606e9e8b5e14236e60f931b67797e20cc1d6b4
Ahsan Zeb <ahsan.zeb@hotmail.com>
2017-09-28 14:35:57 +0100

    working on linblad: routine to init some variables needed for mesolve.

--- fortran-TransOC/src/lindblad.f
+++ fortran-TransOC/src/lindblad.f
@@ -3,9 +3,14 @@
 	implicit none
 	! to use various subroutines in this module
 	integer :: ne, nsec
-	! make it allocatable or parameter and set using modmain's qt.
-	integer, dimension(nsec+1):: ind !start index of sectors
+	integer, allocatable, dimension(:):: ind !start index of sectors
+	double precision, allocatable, dimension(:):: esec, amp
+	double precision, allocatable, dimension(:):: rhov
+	integer, allocatable, dimension(:):: maprho ! location of diagonal elements of rho in rhov
+	integer:: lrhov
 
+
+	
 	! rhovt(:,:): rhov for a coarse grained time grid
 
 	! conversion of deg sectors in rhov back to comput basis
@@ -13,12 +18,7 @@
 
 	! we need to have data for the prev hop so that in case
 	! we are trapped, we pretend to go back and solve master eq etc...
-	itl = mapt%map(itypes(ih,ic));
-	ia = maph(ih,ic); ! location of amplitudes
 
-	!amp, ne,nsec,ind = 
-	!	qt(ia)%cs(ic,is)%amp, eig(itl)%n2, eig(itl)%nsec, eig(itl)%ind
-  
 
 ! Lindblad dynamics
 ! 1. integrate Lindblad equation and find density matrix on a coarse grid
@@ -33,12 +33,43 @@
 !	..... etc.
 
 	contains
+
+!---------------------------------------------
+	subroutine initme(ih,ic,is)
+	! sets gloabal variables of this module
+	use modmain, only: eig,qt,itypes,mapt,maph,mapc
+	implicit none
+	integer, intent(in) :: ih,ic,is
+	!local
+	integer :: itl,icl,ia
+
+	! locations of data
+	itl = mapt%map(itypes(ih,ic)) ! final hilber space
+	icl = mapc(ih,ic); ! channel; reverse of mapc = mapc
+	ia = maph(ih,icl); ! amplitudes
+
+	ne = eig(itl)%n2;
+	nsec = eig(itl)%nsec;
+
+	if(allocated(esec))deallocate(esec)
+	if(allocated(ind))deallocate(ind)
+	if(allocated(amp))deallocate(amp)
+	allocate(esec(nsec))
+	allocate(ind(nsec+1))
+	allocate(amp(ne))
+	
+	esec = eig(itl)%esec(1:nsec);
+	ind = eig(itl)%ind;
+	amp = qt(ia)%cs(icl,is)%amp
+
+	return
+	end 	subroutine initme	
 !---------------------------------------
-	subroutine mesolve(ntot,dt,ntmax)
+	subroutine mesolve(dt,ntmax)
 	implicit none
 	double precision :: dt
 	! aux
-	double precision, dimension(ntot) :: psit,v
+	double precision, dimension(lrhov) :: psit,v
 	integer :: i
 	double precision :: dth,dt6
 
@@ -46,8 +77,12 @@
 	dt6 = dt/6.0d0;
 
 	!	calculate rhov, maprho, lrhov
+	call makerho()
+
 	! calculate gammadec, gammaphi
-	! ne,nsec,ind
+
+
+
 
 	do i=2,ntmax
 		if (mod(i,prntstep) == 0)
@@ -56,43 +91,27 @@
 		call rk4step(psit)
 	end do
 
-	contains
-      !----------------------------------------
-      subroutine yprime(vin,vout)
-      implicit none
-      double precision, intent(in) :: vin(ntot)
-      double precision, intent(out) :: vout(ntot)
-			call dissipators(ne,nsec,ind,vin,vout)
-			return
-      end subroutine yprime
-      !----------------------------------------
-      subroutine rk4step(y)
-      implicit none
-      double precision, intent(inout) :: y(ntot)
-      ! local
-			double precision, dimension(ntot) :: k1,k2,k3    
-      call yprime(y,k1)
-      call yprime(y+k1*dth,k2)
-      k1 = k1 + 2*k2
-      call yprime(y+k2*dth,k3)
-      k1 = k1 + 2*k3
-      call yprime(y+k3*dt,k2)
-      k1 = k1 + k2
-      y = y + k1*dt6
-      end subroutine rk4step
-      !----------------------------------------
 	end subroutine mesolve
 !---------------------------------------
-	subroutine makerho(amp,ne,nsec,ind)
-	use modmain, only: rhov, maprho, lrhov
+	subroutine rk4step(y)
 	implicit none
-	! out: sets global arrays wij, rhov, maprho,
-	!											 and size of rhov = lrhov
-	integer, intent(in) :: ne, nsec
-	double precision,dimension(ne),intent(in):: amp
-	integer,dimension(nsec+1),intent(in):: ind !start index of sectors
-	!integer, intent(out) :: ntot ! size of rhov array
-
+	double precision, intent(inout) :: y(lrhov)
+	! local
+	double precision, dimension(lrhov) :: k1,k2,k3   
+	call yprime(y,k1)
+	call yprime(y+k1*dth,k2)
+	k1 = k1 + 2*k2
+	call yprime(y+k2*dth,k3)
+	k1 = k1 + 2*k3
+	call yprime(y+k3*dt,k2)
+	k1 = k1 + k2
+	y = y + k1*dt6
+	return
+	end subroutine rk4step
+!----------------------------------------
+	subroutine makerho() !in: amp,ne,nsec,ind
+	implicit none
+	! out: sets global arrays rhov, maprho, and size of rhov = lrhov
 	! local
 	integer:: i,i1,i2,j,k,indx
 
@@ -129,20 +148,16 @@
 	return
 	subroutine makerho
 !---------------------------------------
-	subroutine dissipators(ne,nsec,ind,rhov,Ldiss) ! yprime
+	subroutine yprime(rhov,Ldiss) ! dissipators
 	! calculates Ldiss using input rhov
-	use modmain, only: maprho, lrhov,
-     .               Ldiss, gammadec, gammaphi
+	use modmain, only: gammadec, gammaphi ! define these in this module?
 	implicit none
-	integer, intent(in) :: ne, nsec
-	integer,dimension(nsec+1),intent(in):: ind !start index of sectors
 	double precision, dimension(lrhov),intent(in):: rhov
 	double precision, dimension(lrhov),intent(out):: Ldiss
 	
 	! local
-	!double precision, dimension(lrhov):: Lphi
 	double precision, dimension(ne):: Ldecay
-	integer:: i,i1,i2,j,k,indx,ntot,g,is,js
+	integer:: i,i1,i2,j,k,indx,lrhov,g,is,js
 
 	! NOTE:
 	! calculate gammadec, gammaphi before calling this.
@@ -181,7 +196,7 @@
 	enddo
 
 	return
-	subroutine dissipators ! yprime
+	subroutine yprime
 !---------------------------------------
 
 
